41dd2a7c4984f7d67079d1f30be43bfc2d917a4c
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-11-11 23:57:58 +0900

    Hello workd added

--- sample.asm
+++ sample.asm
@@ -1,5 +1,19 @@
-global  _main
-extern  _puts
-section .text
+;
+; This code works on Mac OSX 10.13.x
+;
+  global  _main
+  extern  _puts
+  section .text
+
 _main:
-        ret
+  sub     rsp, 8
+  lea     rdi, [message]
+  call    _puts
+  add     rsp, 8
+  ret
+
+  section .data
+
+message:
+default   rel
+  db      "Hello World!", 0
